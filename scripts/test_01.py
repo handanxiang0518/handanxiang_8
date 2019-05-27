ad677eadf8a3fd09c979b76b3bcfc78dadcde7c5
handanxiang0518 <1724730804@qq.com>
2019-05-27 14:39:06 +0800

    新建文件3

---
+++ scripts/test_01.py
@@ -0,0 +1,10 @@
+import allure, pytest
+
+class Test_01:
+
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    @allure.step(title="这是一个测试步骤01")
+    def test_01_1(self):
+        print("--->test_001_1")
+        allure.attach("标题", "具体内容")
+        assert True
